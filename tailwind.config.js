02df69dea0ab5ec0ad585eea47bee9e381f4084a
AnthonyArade <anthonyarade@gmail.com>
2025-10-24 09:16:46 -0400

    Initialisation of Breeze

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,21 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-  ],
-  safelist: [
-    {
-      pattern: /bg-(gray|blue|pink|green|yellow|purple|red|amber|indigo)-(100|200|300|400|500|600|700|800|900)/,
+export default {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+        },
     },
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+
+    plugins: [forms],
 };
